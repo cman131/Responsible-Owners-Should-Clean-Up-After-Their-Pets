6bc668cbf0ad5dd942501f15fbb3064cf1334bb1
cman131 <conor13@msn.com>
2026-09-14 18:13:54 -0400

    feat: add lockedReason to ActionRequestPayload

--- packages/shared/src/types/events.ts
+++ packages/shared/src/types/events.ts
@@ -76,6 +76,7 @@ export interface ActionRequestPayload {
   canSwitch: boolean;
   switchTargets: string[];
   canTerastallize: boolean;
+  lockedReason?: 'recharge' | 'sleep' | 'freeze';
 }
 
 export interface TurnResolveEvent {
